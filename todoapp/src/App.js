5293be6286aaa6e4d7a2020e04ad5343ca649cdf
CodeToolBox2022 <joshiujjan41@gmail.com>
2022-11-09 08:11:31 +0545

    Project

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
-
+import "./App.css"
 const App=()=>{
   const[userInput,setUserInput] = useState("")
   const [todos,setTodos] = useState([ ])
   const addData = ()=>{
     setTodos([...todos,userInput])
-    setUserInput("")
   }
 return <div>
-  <center>
     <h1>TODO List</h1>
+    <span>
     <input onChange={(event)=> setUserInput(event.target.value)}type="Text"></input>
     <button onClick={()=>addData()}>Add</button>
+   
     <ul>
       {
         todos.map((todo,index)=> <li key={index}>{todo}</li>)
       }
     </ul>
-    </center>
+    </span>
   </div>
   }
 
